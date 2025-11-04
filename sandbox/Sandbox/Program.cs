f133f0b4e1f68dc7a3dc475ea03786bb6fdb3d88
CyleA25088 <235726447+CyleA25088@users.noreply.github.com>
2025-11-04 09:44:45 -0700

    update

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine(Math.Pow(2, 10));
+        int x = 10;
+        Console.WriteLine($"{x}, {++x}, {x++}, {x}");
+
+        for (int i = -1000; i < 1000; i += 10)
+        {
+            Console.WriteLine($"The value of i is: {i}");
+        }
     }
 }
