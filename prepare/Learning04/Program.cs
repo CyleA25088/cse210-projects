00d481c8c290c7e726c4b5dfa3690bb72f0d5fb8
CyleA25088 <235726447+CyleA25088@users.noreply.github.com>
2025-11-18 09:33:55 -0700

    W07 prepare learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Bob Douglas", "What is DNA");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Bob Dilly", "Fractions", "1.1", "1-20");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Bob Striker", "History", "The Holodomor");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
