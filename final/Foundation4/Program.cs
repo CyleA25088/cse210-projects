df0f198fa16c78addfa76d46e29ede29acef0bf0
CyleA25088 <235726447+CyleA25088@users.noreply.github.com>
2025-12-08 10:00:51 -0700

    Add activity tracking classes and summaries
    Introduced abstract Activity base class and concrete Running, Cycling, and Swimming classes with calculation methods for distance, speed, and pace. Updated Program.cs to generate and display summaries for each activity type.

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
+    
+    static List<Activity> activities = new List<Activity>();
+
+    static private void GenerateActivities()
+    {
+        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 3.0));
+        activities.Add(new Cycling(new DateTime(2022, 11, 3), 30, 6.0));
+        activities.Add(new Swimming(new DateTime(2022, 11, 3), 30, 40));}
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        GenerateActivities();
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
---
+++ final/Foundation4/Swimming.cs
@@ -0,0 +1,27 @@
+class Swimming : Activity
+{
+    private int _laps;
+
+    public Swimming(DateTime date, int minutes, int laps)
+        : base(date, minutes)
+    {
+        _laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        double kilometers = (_laps * 50) / 1000.0;
+        double miles = kilometers * 0.62;
+        return miles;
+    }
+
+    public override double GetSpeed()
+    {
+        return (GetDistance() / Minutes) * 60;
+    }
+
+    public override double GetPace()
+    {
+        return Minutes / GetDistance();
+    }
+}
